5b89bc1ece0f4583fb346ab8e160e8fb65ca5e58
pobb <79351885+1506086927@users.noreply.github.com>
2026-05-10 21:10:18 +0800

    Update CC-HH.cmd

--- CC-HH.cmd
+++ CC-HH.cmd
@@ -5,7 +5,7 @@ chcp 65001 >nul
 :: 强制声明终端环境为 UTF-8，解决 Bun 等现代 CLI 工具的底层输出乱码问题
 set LANG=zh_CN.UTF-8
 set LC_ALL=zh_CN.UTF-8
-set "CLAUDE_CONFIG_DIR=你的目录\CLAUDE_CONFIG_DIR"
+set "CLAUDE_CONFIG_DIR=doge-code目录\CLAUDE_CONFIG_DIR"
 :: 获取正确的中文路径
 set "TARGET_DIR=%cd%"
 
@@ -16,7 +16,7 @@ set path=%PYTHON%;%PYTHONScripts%;%path%
 set CLAUDE_CODE_USE_POWERSHELL_TOOL=1
 set PLAYWRIGHT_BROWSERS_PATH=D:\py-project\python\ms-playwright
 :: 切换到项目目录
-cd /d "你的目录"
+cd /d "doge-code目录"
 
 :: 启动 Bun
 D:\java-project\bun-windows-x64\bun.exe --env-file=.env --preload ./set-cwd.ts ./src/entrypoints/cli.tsx %*
